e2440de17577e9da05d4975c1851dece7c56b14d
Maarten van der Peet <maartenp@miniMaart.local>
2019-10-29 21:42:59 +0100

    cleanup and start Spinningballs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
+
 import './stopwatch.css';
 
 import Timer from './components/Timer'
@@ -11,169 +10,96 @@ import { time2seconds, seconds2time } from './components/tijdfuncties'
 
 class App extends React.Component {
 
-  // static buttons = ['start', 'wait', 'continue', 'stop'];
+    constructor(props) {
+        super(props);
+        this.state = {
+            time: '6:00',
+            statustext: 'Ready to Go',
+            starttime: '6:00',
+            activebutton: '',
+            running: false
+        };
+        this.buttons = ['start', 'wait', 'continue', 'stop'];
+        this.statuslines = ['Running', 'Interupted', 'Running again', 'Stopped'];
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      time: '6:00',
-      statustext: 'Ready to Go',
-      starttime: '6:00',
-      // buttons: ['start', 'wait', 'continue', 'stop'],
-      activebutton: '',
-      running: false
-    };
-    this.buttons = ['start', 'wait', 'continue', 'stop'];
-    this.statuslines = ['Running', 'Interupted', 'Running again', 'Stopped'];
-
-    this.verwerkKlik = this.verwerkKlik.bind(this);
-    this.changeStartTime = this.changeStartTime.bind(this);
-    this.validateTimeOnBlur = this.validateTimeOnBlur.bind(this);
-    // this.determineStartTime();
-
-    //  console.log(this.state.activeButton);
-
-  }
-
-  componentDidMount() {
-    if(localStorage.getItem('initt')) {
-      let initt = localStorage.getItem('initt');
-        document.getElementById("inittime").value = initt; // only possible after everything mounted
-        this.setState({ time: initt });
-        this.setState({ starttime: initt });
-     }  else if (document.getElementById("inittime").value) {
-      this.setState({ time: document.getElementById("inittime").value });
-      this.setState({ starttime: document.getElementById("inittime").value });
-      localStorage.setItem('initt', document.getElementById("inittime").value);
-     }
-      
-
-    // } else {
-    //   this.setState({ time: '16:00' })
-    // }
-
-  }
-
-
-  verwerkKlik(e) {
-    console.log(e.target.value);
-    console.log('verwerkKlik ' + e);
-    // this.setState({activebutton: staat});
-    // this.forceUpdate();
-    // this.setState((state,e) => ({
-    //   activebutton: e.target.value
-    // }));
-    // this.setState((state, staat) => ({
-    //   activebutton: staat
-    // }));
-    let nIntervId;
-
-    if (e.target.value === 'start') {
-      // clearInterval(nIntervId);
-      // nIntervId = setInterval(this.updateTime, 1000); // 1000ms == 1 s
-
-      // let currentTime = this.state.time;
-      // let currentSeconds = time2seconds(currentTime) - 1;
-      // let newTime = seconds2time(currentSeconds);
-      // this.setState({time: newTime});
-      this.componentDidMount();
-      clearInterval(this.timer)
-
-      this.timer = setInterval(() => this.updateTime(), 1000);
-
-
-      // this.updateTime();
-    } else {
-      // clearInterval(nIntervId);
-      // this.timer = 0;
-      clearInterval(this.timer)
-      this.setState({ running: false })
-    }
-    this.setState({ activebutton: e.target.value }, function () {
-      console.log(this.state.activebutton);
-      // this.forceUpdate();
-    });
-    const i = this.buttons.indexOf(e.target.value);
-    const status = this.statuslines[i];
-
-    // this.setState({ statustext: status }, function () {
-    //   console.log(this.state.statustext);
-    //   // this.forceUpdate();
-    // });
-
-    this.setState({ statustext: status });
-
-
-    // HIERBOVEN EEN FUNCTIE OMDAT DAT NIET CORRECT UPDATE, maar niet nodig bij changStartTime??? WTF
-    // NOu ja nu lukt dat weer wel...
-
-    // console.log('na zetten ' + this.state.activebutton);
-    // this.componentDidUpdate();
-  }
-
-  updateTime() {
-
-    // this.setState({ running: true });
-
-
-    let currentTime = this.state.time;
-    let currentSeconds = time2seconds(currentTime) - 1;
-    let newTime = seconds2time(currentSeconds);
-    this.setState({ time: newTime });
-
-    console.log(currentSeconds);
-    // return currentSeconds;
-
-
-  }
-
-  // twee eventhandlers, 1 voor de invoer, 1 voor validering, je wil alleen valideren op het verlaten van het veld
-  changeStartTime(e) {
-    console.log(e);
-    this.setState({ starttime: e.target.value });
-    this.setState({ time: e.target.value });
-    localStorage.setItem('initt', document.getElementById("inittime").value);
-
-  }
-
-  validateTimeOnBlur(e) {
-    const re = /^\d\d?:\d\d$/;
-    // heel precies de boundaries aangeven.
-    console.log('validate test: ' + e.target.value)
-    if (re.test(e.target.value)) {
-      console.log('validate: yep dus ')
-
-      return true;
-    } else {
-      // this.setState({ starttime: '6:00' });
-      this.setState({ time: '6:00' });
-
+        this.verwerkKlik = this.verwerkKlik.bind(this);
+        this.changeStartTime = this.changeStartTime.bind(this);
+        this.validateTimeOnBlur = this.validateTimeOnBlur.bind(this);
     }
 
-  }
+    componentDidMount() {
+        if (localStorage.getItem('initt')) {
+            let initt = localStorage.getItem('initt');
+            document.getElementById("inittime").value = initt; // only possible after everything mounted
+            this.setState({ time: initt });
+            this.setState({ starttime: initt });
+        } else if (document.getElementById("inittime").value) {
+            this.setState({ time: document.getElementById("inittime").value });
+            this.setState({ starttime: document.getElementById("inittime").value });
+            localStorage.setItem('initt', document.getElementById("inittime").value);
+        }
+        // } else {
+        //   this.setState({ time: '16:00' })
+        // }
+    }
+    verwerkKlik(e) {
+        console.log(e.target.value);
+        console.log('verwerkKlik ' + e);
+        if (e.target.value === 'start') {
+            this.componentDidMount();
+            clearInterval(this.timer)
+            this.timer = setInterval(() => this.updateTime(), 1000);
+        } else {
+            clearInterval(this.timer)
+            this.setState({ running: false })
+        }
+        this.setState({ activebutton: e.target.value }, function () {
+            console.log(this.state.activebutton);
+        });
+        const i = this.buttons.indexOf(e.target.value);
+        const status = this.statuslines[i];
+        this.setState({ statustext: status });
+    }
 
+    updateTime() {
+        let currentTime = this.state.time;
+        let currentSeconds = time2seconds(currentTime) - 1;
+        let newTime = seconds2time(currentSeconds);
+        this.setState({ time: newTime });
+    }
 
-  ValidTime(value) {
-    const re = /\d:\d\d/;
+    // twee eventhandlers, 1 voor de invoer, 1 voor validering, je wil alleen valideren op het verlaten van het veld
+    changeStartTime(e) {
+        console.log(e);
+        this.setState({ starttime: e.target.value });
+        this.setState({ time: e.target.value });
+        localStorage.setItem('initt', document.getElementById("inittime").value);
+    }
 
-    return true;
-    // if(re.test(value)) {
-    //     return true;
-    // } else {
-    //   return false;
-    // }
-  }
-  render() {
-    return (
-      <div id="wrap">
-        <Timer time={this.state.time} state={this.state.activebutton} />
-        <Knoppenbord action={this.verwerkKlik} buttons={this.buttons} activebutton={this.state.activebutton} />
-        <Statusline statustext={this.state.statustext} />
-        <Configuration starttime={this.state.starttime} action={this.changeStartTime} validate={this.validateTimeOnBlur} />
-        <Spinningbal running={this.state.running} />
-      </div>
-    );
-  }
+    validateTimeOnBlur(e) {
+        const re = /^\d\d?:\d\d$/;
+        // heel precies de boundaries aangeven.
+        console.log('validate test: ' + e.target.value)
+        if (re.test(e.target.value)) {
+            console.log('validate: yep dus ')
+            return true;
+        } else {
+            // this.setState({ starttime: '6:00' });
+            this.setState({ time: '6:00' });
+        }
+    }
+
+    render() {
+        return (
+            <div id="wrap">
+                <Timer time={this.state.time} state={this.state.activebutton} />
+                <Knoppenbord action={this.verwerkKlik} buttons={this.buttons} activebutton={this.state.activebutton} />
+                <Statusline statustext={this.state.statustext} />
+                <Configuration starttime={this.state.starttime} action={this.changeStartTime} validate={this.validateTimeOnBlur} />
+                <Spinningbal activebutton={this.state.activebutton} time={this.state.time} />
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/components/Statusline.js
+++ src/components/Statusline.js
@@ -14,10 +14,22 @@ class Statusline extends React.Component {
 
 
 function Spinningbal(props) {
-    if(props.running === true) {
-    return <div className="spinningbal running">R</div>
+    console.log('time', props.time);
+    let time = props.time.split(":");
+    // console.log(time);
+    let staticcoin = Number(time[0]);
+    let staticballs = 'COIN '.repeat(staticcoin);
+
+    let num = staticcoin ;
+    let balls = 'COIN '.repeat(num);
+    console.log(balls);
+    let testletter = 'q';
+
+  
+    if(props.activebutton === 'start') {
+        return (<div className="spinningbal running">{balls }</div>)
     } else {
-        return <div className="spinningbal">r</div>
+        return (<div className="spinningbal">{balls }</div>)
     }
 }
 
